78e33041a31b85816ef8da4f5513bc50c6de5116
mrsimpson <github@beimir.net>
2016-05-10 17:28:42 +0200

    Configuration options for Reisebuddy

---
+++ packages/reisebuddy-livechat/package.js
@@ -0,0 +1,12 @@
+/* globals Package */
+Package.describe({
+	name: 'reisebuddy:livechat',
+	version: '0.0.1',
+	summary: '',
+	git: '',
+	documentation: null
+});
+
+Package.onUse(function(api) {
+	api.addFiles('server/startup/settings.js', 'server');
+});
