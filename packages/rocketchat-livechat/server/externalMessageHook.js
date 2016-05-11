df0b0f3c4f9d6b7c0e5033f2b17012c4e074177e
mrsimpson <github@beimir.net>
2016-05-11 21:46:43 +0200

    Redlink-Integration call prepare

--- packages/rocketchat-livechat/server/externalMessageHook.js
+++ packages/rocketchat-livechat/server/externalMessageHook.js
@@ -3,14 +3,29 @@
 var knowledgeEnabled = false;
 var apiaiKey = '';
 var apiaiLanguage = 'en';
+var knowledgeSource = '';
+var redlinkURL = '';
+var redlinkAuthToken = '';
+
+const KNOWLEDGE_SRC_APIAI = "0";
+const KNOWLEDGE_SRC_REDLINK = "1";
 RocketChat.settings.get('Livechat_Knowledge_Enabled', function(key, value) {
 	knowledgeEnabled = value;
+});
+RocketChat.settings.get('Livechat_Knowledge_Source', function(key, value) {
+	knowledgeSource = value;
 });
 RocketChat.settings.get('Livechat_Knowledge_Apiai_Key', function(key, value) {
 	apiaiKey = value;
 });
 RocketChat.settings.get('Livechat_Knowledge_Apiai_Language', function(key, value) {
 	apiaiLanguage = value;
+});
+RocketChat.settings.get('Livechat_Knowledge_Redlink_URL', function(key, value) {
+	redlinkURL = value;
+});
+RocketChat.settings.get('Livechat_Knowledge_Redlink_Auth_Token', function(key, value) {
+	redlinkAuthToken = value;
 });
 
 RocketChat.callbacks.add('afterSaveMessage', function(message, room) {
@@ -33,28 +48,67 @@ RocketChat.callbacks.add('afterSaveMessage', function(message, room) {
 	}
 
 	Meteor.defer(() => {
-		try {
-			const response = HTTP.post('https://api.api.ai/api/query?v=20150910', {
-				data: {
-					query: message.msg,
-					lang: apiaiLanguage
-				},
-				headers: {
-					'Content-Type': 'application/json; charset=utf-8',
-					'Authorization': 'Bearer ' + apiaiKey
+		switch( knowledgeSource ) {
+		case KNOWLEDGE_SRC_APIAI:
+				try {
+					const responseAPIAI = HTTP.post('https://api.api.ai/api/query?v=20150910', {
+						data: {
+							query: message.msg,
+							lang: apiaiLanguage
+						},
+						headers: {
+							'Content-Type': 'application/json; charset=utf-8',
+							'Authorization': 'Bearer ' + apiaiKey
+						}
+					})
+					if (responseAPIAI.data && responseAPIAI.data.status.code === 200 && !_.isEmpty(responseAPIAI.data.result.fulfillment.speech)) {
+						RocketChat.models.LivechatExternalMessage.insert({
+							rid: message.rid,
+							msg: responseAPIAI.data.result.fulfillment.speech,
+							orig: message._id,
+							ts: new Date()
+						});
+					}
 				}
-			});
+				catch(e) {
+					SystemLogger.error('Error using Api.ai ->', e);
+				}
+			break;
+		case KNOWLEDGE_SRC_REDLINK:
+				try {
+					var responseRedlink = HTTP.post(redlinkURL + '/prepare', {
+						data: {
+								messages: [
+									{
+										content: message.msg
+									}
+								]
+						},
+						headers: {
+							'Content-Type': 'application/json; charset=utf-8',
+							'Authorization': 'basic ' + redlinkAuthToken
+						}
+					})
 
-			if (response.data && response.data.status.code === 200 && !_.isEmpty(response.data.result.fulfillment.speech)) {
-				RocketChat.models.LivechatExternalMessage.insert({
-					rid: message.rid,
-					msg: response.data.result.fulfillment.speech,
-					orig: message._id,
-					ts: new Date()
-				});
-			}
-		} catch (e) {
-			SystemLogger.error('Error using Api.ai ->', e);
+					if (responseRedlink.data && responseRedlink.statusCode === 200) {
+						for (var i = 0; i < responseRedlink.data.queries.length; i++){
+							RocketChat.models.LivechatExternalMessage.insert({
+								rid: message.rid,
+								msg: responseRedlink.data.queries[i].serviceName,
+								url: responseRedlink.data.queries[i].url,
+								orig: message._id,
+								ts: new Date()
+							});
+						}
+					}
+				} catch (e) {
+					SystemLogger.error('Error using Redlink ->', e);
+				}
+
+			break;
+		default:
+			//don't set neither msg nor url
+			break;
 		}
 	});
 
