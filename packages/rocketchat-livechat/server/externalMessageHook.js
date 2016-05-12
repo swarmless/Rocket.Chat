69111f126ad7c375ae90fc5e7b7e4c0069b126bb
Oliver Jägle <github@beimir.net>
2016-05-12 11:20:24 +0200

    Knowledge-Base refactored

--- packages/rocketchat-livechat/server/externalMessageHook.js
+++ packages/rocketchat-livechat/server/externalMessageHook.js
@@ -28,6 +28,66 @@ RocketChat.settings.get('Livechat_Knowledge_Redlink_Auth_Token', function(key, v
 	redlinkAuthToken = value;
 });
 
+var getKnowledgeAdapter = function(knowledgeSource){
+	switch( knowledgeSource ) {
+		case KNOWLEDGE_SRC_APIAI:
+			return {
+				onMessage: function(message){
+					const responseAPIAI = HTTP.post('https://api.api.ai/api/query?v=20150910', {
+						data: {
+							query: message.msg,
+							lang: apiaiLanguage
+						},
+						headers: {
+							'Content-Type': 'application/json; charset=utf-8',
+							'Authorization': 'Bearer ' + apiaiKey
+						}
+					});
+					if (responseAPIAI.data && responseAPIAI.data.status.code === 200 && !_.isEmpty(responseAPIAI.data.result.fulfillment.speech)) {
+						RocketChat.models.LivechatExternalMessage.insert({
+							rid: message.rid,
+							msg: responseAPIAI.data.result.fulfillment.speech,
+							orig: message._id,
+							ts: new Date()
+						});
+					}
+				}
+			};
+		break;
+		case KNOWLEDGE_SRC_REDLINK:
+			return {
+				onMessage: function(message){
+					//todo: Alle Nachrichten des Raums abholen
+					var responseRedlink = HTTP.post(redlinkURL + '/prepare', {
+						data: {
+							messages: [
+								{
+									content: message.msg
+								}
+							]
+						},
+						headers: {
+							'Content-Type': 'application/json; charset=utf-8',
+							'Authorization': 'basic ' + redlinkAuthToken
+						}
+					})
+
+					if (responseRedlink.data && responseRedlink.statusCode === 200) {
+						for (var i = 0; i < responseRedlink.data.queries.length; i++){
+							RocketChat.models.LivechatExternalMessage.insert({
+								rid: message.rid,
+								msg: responseRedlink.data.queries[i].serviceName,
+								url: responseRedlink.data.queries[i].url,
+								orig: message._id,
+								ts: new Date()
+							});
+						}
+					}
+				}
+			}
+	}
+};
+
 RocketChat.callbacks.add('afterSaveMessage', function(message, room) {
 	// skips this callback if the message was edited
 	if (message.editedAt) {
@@ -47,69 +107,18 @@ RocketChat.callbacks.add('afterSaveMessage', function(message, room) {
 		return message;
 	}
 
+	const knowledgeAdapter = getKnowledgeAdapter(knowledgeSource);
+	if (!knowledgeAdapter) {
+		return;
+	}
+
 	Meteor.defer(() => {
-		switch( knowledgeSource ) {
-		case KNOWLEDGE_SRC_APIAI:
 				try {
-					const responseAPIAI = HTTP.post('https://api.api.ai/api/query?v=20150910', {
-						data: {
-							query: message.msg,
-							lang: apiaiLanguage
-						},
-						headers: {
-							'Content-Type': 'application/json; charset=utf-8',
-							'Authorization': 'Bearer ' + apiaiKey
-						}
-					})
-					if (responseAPIAI.data && responseAPIAI.data.status.code === 200 && !_.isEmpty(responseAPIAI.data.result.fulfillment.speech)) {
-						RocketChat.models.LivechatExternalMessage.insert({
-							rid: message.rid,
-							msg: responseAPIAI.data.result.fulfillment.speech,
-							orig: message._id,
-							ts: new Date()
-						});
-					}
+					getKnowledgeAdapter(knowledgeSource).onMessage(message);
 				}
 				catch(e) {
-					SystemLogger.error('Error using Api.ai ->', e);
+					SystemLogger.error('Error using knowledge provider ->', e);
 				}
-			break;
-		case KNOWLEDGE_SRC_REDLINK:
-				try {
-					var responseRedlink = HTTP.post(redlinkURL + '/prepare', {
-						data: {
-								messages: [
-									{
-										content: message.msg
-									}
-								]
-						},
-						headers: {
-							'Content-Type': 'application/json; charset=utf-8',
-							'Authorization': 'basic ' + redlinkAuthToken
-						}
-					})
-
-					if (responseRedlink.data && responseRedlink.statusCode === 200) {
-						for (var i = 0; i < responseRedlink.data.queries.length; i++){
-							RocketChat.models.LivechatExternalMessage.insert({
-								rid: message.rid,
-								msg: responseRedlink.data.queries[i].serviceName,
-								url: responseRedlink.data.queries[i].url,
-								orig: message._id,
-								ts: new Date()
-							});
-						}
-					}
-				} catch (e) {
-					SystemLogger.error('Error using Redlink ->', e);
-				}
-
-			break;
-		default:
-			//don't set neither msg nor url
-			break;
-		}
 	});
 
 	return message;
