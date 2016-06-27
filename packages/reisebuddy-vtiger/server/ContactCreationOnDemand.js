cc67b9c2f621ae395a7d722fb3e603dcea0e60a5
mrsimpson <github@beimir.net>
2016-06-27 09:14:22 +0200

    [13] Als Anwendungsinhaber möchte ich, dass ein Kunde, der noch nicht im CRM-System vorhanden ist, als Neukunde im CRM angelegt wird.
    Automatische Neuanlage bei erster Nachricht implementiert.

--- packages/reisebuddy-vtiger/server/ContactCreationOnDemand.js
+++ packages/reisebuddy-vtiger/server/ContactCreationOnDemand.js
@@ -1,3 +1,10 @@
-RocketChat.callbacks.add('afterCreateChannel', (room) => {
-	console.log(room);
+RocketChat.callbacks.add('afterCreateLivechat', (guest, room) => {
+
+	if (!guest.crmContactId) {
+		Meteor.call('livechat:createCrmContact', guest, (err, data)=> {
+			if (err) {
+				console.error(err);
+			}
+		});
+	}
 });
