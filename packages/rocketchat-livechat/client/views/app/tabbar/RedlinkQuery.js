d2ab3e038ad046e695db30a78f6e7e0ccef8f0f8
mrsimpson <github@beimir.net>
2016-07-12 13:02:56 +0200

    Redlink-Integration: Inline results display (unformated)

--- packages/rocketchat-livechat/client/views/app/tabbar/RedlinkQuery.js
+++ packages/rocketchat-livechat/client/views/app/tabbar/RedlinkQuery.js
@@ -3,23 +3,66 @@ Template.redlinkQuery.helpers({
 		return "icon-link-ext";
 	},
 
-	results(){
+	visibleResult(){
 		const instance = Template.instance();
-		return instance.state.get('results');
+		const results = instance.state.get('results');
+		if(results) return results[instance.state.get('visibleResultIndex')];
+	},
+
+	resultsCount(){
+		const instance = Template.instance();
+		const results = instance.state.get('results');
+		if(results) return results.length;
+	},
+
+	visibleResultNumer(){
+		const visibleResultIndex = instance.state.get('visibleResultIndex');
+		return visibleResultIndex + 1;
+	},
+
+	classExpanded(){
+		const instance = Template.instance();
+		return instance.state.get('resultsExpanded') ? 'expanded' : 'collapsed';
 	}
 });
 
-Template.redlinkQuery.events({});
+Template.redlinkQuery.events({
+	'click .js-toggle-results-expanded': function(event, instance){
+		const current = instance.state.get('resultsExpanded');
+		instance.state.set('resultsExpanded', !current);
+	},
+
+	'click .js-next-result': function(event, instance){
+		const visibleResultIndex = instance.state.get('visibleResultIndex');
+		const results = instance.state.get('results');
+		if(visibleResultIndex < results.length - 1){
+			instance.state.set('visibleResultIndex', visibleResultIndex + 1);
+		} else {
+			instance.state.set('visibleResultIndex', 0);
+		}
+	},
+
+	'click .js-previous-result': function(event, instance){
+		const visibleResultIndex = instance.state.get('visibleResultIndex');
+		const results = instance.state.get('results');
+		if(visibleResultIndex > 0 ){
+			instance.state.set('visibleResultIndex', visibleResultIndex - 1);
+		} else {
+			instance.state.set('visibleResultIndex', results.length - 1);
+		}
+	}
+});
 
 Template.redlinkQuery.onCreated(function () {
+	const instance = this;
+
 	this.state = new ReactiveDict();
 	this.state.setDefault({
-		expanded: false,
+		expanded: instance.data.query.inlineResultSupport && ( instance.data.maxConfidence === instance.data.query.confidence ),
 		results: [],
-		resultsFetched: false
+		resultsFetched: false, // in order to be able to determine an empty result list from not having tried to fetch results
+		visibleResultIndex: 0
 	});
-
-	const instance = this;
 
 	// Asynchronously load the results.
 	Meteor.defer(()=>{
