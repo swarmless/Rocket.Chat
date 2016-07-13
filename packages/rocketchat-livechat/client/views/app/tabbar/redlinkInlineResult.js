7daeced33773a78469d906abedc580f6d5e3b13d
mrsimpson <github@beimir.net>
2016-07-13 17:14:45 +0200

    Redlink-Integration: Bahn.de Template inline result data populated

--- packages/rocketchat-livechat/client/views/app/tabbar/redlinkInlineResult.js
+++ packages/rocketchat-livechat/client/views/app/tabbar/redlinkInlineResult.js
@@ -1,3 +1,10 @@
+Template.redlinkInlineResult._copyReplySuggestion = function(event, instance)
+{
+	if (instance.data.result.replySuggestion) {
+		$('#chat-window-' + instance.data.roomId + ' .input-message').val(instance.data.result.replySuggestion);
+	}
+}
+
 Template.redlinkInlineResult.helpers({
 	templateName(){
 		const instance = Template.instance();
@@ -5,7 +12,7 @@ Template.redlinkInlineResult.helpers({
 		let templateSuffix = "generic";
 		switch (instance.data.result.creator) {
 			case 'bahn.de':
-				templateSuffix = "bahn.de";
+				templateSuffix = "bahn_de";
 				break;
 			case 'community.bahn.de':
 				templateSuffix = "VKL_community";
@@ -18,31 +25,37 @@ Template.redlinkInlineResult.helpers({
 				break;
 		}
 		return 'redlinkInlineResult_' + templateSuffix;
+	},
+	templateData(){
+		const instance = Template.instance();
+		return {
+			result: instance.data.result,
+			roomId: instance.data.roomId
+		}
 	}
 });
 
 Template.redlinkInlineResult.events({
 	'click .js-copy-reply-suggestion': function (event, instance) {
-		if (instance.data.result.replySuggestion) {
-			$('#chat-window-' + instance.data.roomId + ' .input-message').val(instance.data.result.replySuggestion);
-		}
+		return Template.redlinkInlineResult._copyReplySuggestion(event, instance)
 	}
 });
 
 Template.redlinkInlineResult_generic.helpers({
-	rawResult(){
-		const instance = Template.instance();
-		return JSON.stringify(instance.data, ' ', 2);
-	},
-
 	relevantKeyValues(){
 		const instance = Template.instance();
 
 		let keyValuePairs = [];
-		for (key in instance.data) {
+		for (key in instance.data.result) {
 			keyValuePairs.push({key: key, value: instance.data.result[key]});
 		}
 
 		return keyValuePairs;
+	}
+});
+
+Template.redlinkInlineResult_bahn_de.events({
+	'click .js-copy-reply-suggestion': function(event, instance){
+		return Template.redlinkInlineResult._copyReplySuggestion(event, instance)
 	}
 });
