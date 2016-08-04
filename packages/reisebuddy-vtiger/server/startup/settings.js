8390c9ec5a1b584c8b119a88120d69832fdb08d4
JakobPannier <jakob.pannier@deutschebahn.com>
2016-08-04 14:18:06 +0200

    moved crm-settings to reisebuddy-group

--- packages/reisebuddy-vtiger/server/startup/settings.js
+++ packages/reisebuddy-vtiger/server/startup/settings.js
@@ -1,30 +1,32 @@
 Meteor.startup(function() {
-	RocketChat.settings.addGroup('CRM');
-
 	RocketChat.settings.add('CRM_vtiger_url', true, {
 		type: 'string',
-		group: 'CRM',
+		group: 'Reisebuddy',
+		section: 'CRM',
 		public: true,
 		i18nLabel: 'DBRB_vtiger_url'
 	});
 
 	RocketChat.settings.add('CRM_vtiger_username', true, {
 		type: 'string',
-		group: 'CRM',
+		group: 'Reisebuddy',
+		section: 'CRM',
 		public: true,
 		i18nLabel: 'DBRB_vtiger_username'
 	});
 
 	RocketChat.settings.add('CRM_vtiger_user_id', true, {
 		type: 'string',
-		group: 'CRM',
+		group: 'Reisebuddy',
+		section: 'CRM',
 		public: true,
 		i18nLabel: 'DBRB_vtiger_user_id'
 	});
 
 	RocketChat.settings.add('CRM_vtiger_userAccessKey', true, {
 		type: 'string',
-		group: 'CRM',
+		group: 'Reisebuddy',
+		section: 'CRM',
 		public: true,
 		i18nLabel: 'DBRB_vtiger_userAccessKey'
 	});
